47994c7008c5755b9948aa1969b0713d565402c9
siddharth-bijjaragi <123967529+siddharth-bijjaragi@users.noreply.github.com>
2023-08-08 10:50:45 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "87dbe6efbfef7574f629719ab5b563d9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
